3d6598e794889c4984ec7f371218721f64e4c1b7
Nimesh Maharjan <maharjannimesh11@gmail.com>
2025-06-17 17:45:30 +0545

    eslint diagnostics and telescope hidden files

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      file_ignore_patterns = {},
+      hidden = true,
+    },
+    pickers = {
+      find_files = {
+        hidden = true,
+      },
+    },
+  },
+}
